66ca7523b9f516b2b72f696ac9153ffda5d4be4f
kobold <kovacsboldizsar@gmail.com>
2021-06-09 16:05:32 +0300

    Contact form is managed component
    submitting only logs the fields

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,37 @@
+import { useState } from 'react';
 import '../Contact.css'
 
 const Contact = () => {
 
     const submit = (e) => {
-        console.log(e);
+        console.log("Submit pressed \n name=" + name + ", email=" + email + ", message=" + message);
+    }
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
     }
 
     return (
         <div className="contact-page-wrapper">
             <div className="contact-form-wrapper">
                 <div className="left-input">
-                    <input type="text" placeholder="Name" />
-                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Name" value={name} onChange={handleNameChange} />
+                    <input type="text" placeholder="Email" value={email} onChange={handleEmailChange} />
                 </div>
                 <div className="right-input">
-                    <textarea rows="6" cols="30" placeholder="Message (if any)" />
+                    <textarea rows="6" cols="30" placeholder="Message (if any)" value={message} onChange={handleMessageChange} />
                 </div>
             </div>
             <button className="submit-button" onClick={submit}>Submit</button>
